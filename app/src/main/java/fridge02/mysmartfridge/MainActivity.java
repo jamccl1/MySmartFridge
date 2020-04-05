59f9ce3dec9d1369ec647501643c6e2b88f1ce78
jamccl1 <jmmcclellan98@yahoo.com>
2020-04-05 00:42:55 -0400

    Added dummy screens; moved variables to XML files; removed title bar.

--- app/src/main/java/fridge02/mysmartfridge/MainActivity.java
+++ app/src/main/java/fridge02/mysmartfridge/MainActivity.java
@@ -11,13 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        // Set temperature on home screen
+        setHomeScreenTemperature(75);
+    }
+
+    public void toWhatsInFridge(View view) {
+        setContentView(R.layout.whats_in_fridge);
+    }
+
+    public void toShopping(View view) {
+        setContentView(R.layout.shopping);
     }
 
     public void toRecipes(View view) {
         setContentView(R.layout.recipes);
     }
 
-    public void goHome(View view) {
+    public void toLookInside(View view) {
+        setContentView(R.layout.look_inside);
+    }
+
+    public void toHome(View view) {
         setContentView(R.layout.home);
+        setHomeScreenTemperature(75);
+    }
+
+    private void setHomeScreenTemperature(int temperature) {
+        TextView uiTemperature = findViewById(R.id.temperature);
+        uiTemperature.setText(getString(R.string.temperature, temperature));
     }
 }
